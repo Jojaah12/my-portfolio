bc41f40a5a4061dd6c6e473f0621d07a1377a1aa
Jojaah12 <mohammed.aljojah@gmail.com>
2022-09-12 20:03:41 +0200

    added contact from

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,7 +1,15 @@
+import ContactForm from "../../components/ContactForm/ContactForm";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div className="contact">Contact</div>;
+  return (
+    <section className="contact">
+      <SectionTitle sectionTitle="contact" lineColor="#fff" />
+      <p style={{ color: "#4f5656", paddingBottom: "10px" }}>Get In Touch</p>
+      <ContactForm />
+    </section>
+  );
 };
 
 export default Contact;
